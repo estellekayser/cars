12010c6d5b80d03980186dfbd6c0dbe9c3bb41be
estellekayser <67468385+estellekayser@users.noreply.github.com>
2020-10-14 14:52:45 +0200

    MySQL to pd.dataframe

--- cars_sql.py
+++ cars_sql.py
@@ -1,14 +1,22 @@
 import mysql.connector
+import seaborn as sns
+import pandas as pd
 
+# Connexion a MySQL
 cnx = mysql.connector.connect(user='root', password='',
                               host='127.0.0.1',
                               database='cars')
 
 mycursor = cnx.cursor()
 
-mycursor.execute("SELECT * FROM `table 1`")
+# Import table "cars" de MySQL
+mycursor.execute(
+    "SELECT Car_Name, Year, Selling_Price, Present_Price, Kms_Driven, Fuel_Type, Seller_Type, Transmission, Owner \
+    FROM cars")
+request = mycursor.fetchall()
 
-myresult = mycursor.fetchall()
+# Creation de la db
+df = pd.DataFrame([[ij for ij in i] for i in request])
+df.rename(columns={0:'Car_Name', 1:'Year', 2:'Selling_Price', 3:'Present_Price', 4:'Kms_Driven', 5:'Fuel_Type', 6:'Seller_Type', 7:'Transmission', 8:'Owner' }, inplace=True)
 
-for x in myresult:
-  print(x)
+print(df.head)
