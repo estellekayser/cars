0507130aed0a84eef59e659692433311cb1abd51
estellekayser <67468385+estellekayser@users.noreply.github.com>
2020-10-14 16:06:09 +0200

    Add files via upload
    Analyse bivariee

--- cars_sql.py
+++ cars_sql.py
@@ -2,6 +2,7 @@ import mysql.connector
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats.stats import pearsonr
 
 ##### Connexion a MySQL
 cnx = mysql.connector.connect(user='root', password='',
@@ -39,3 +40,19 @@ for var in vquanti:
     plt.title(var)
     plt.show()
 
+##### Analyse bivariee
+pd.plotting.scatter_matrix(df)
+plt.show()
+# On suppose une correlation entre Present_Price et Selling_Price
+
+coeff_pearson,_ = pearsonr(df['Present_Price'],df['Selling_Price'])
+print("coefficient de Pearson = {}".format(coeff_pearson))
+#  coeff_pearson : 0.88 --> Correlation positive
+
+coeff_pearson,_ = pearsonr(df['Kms_Driven'],df['Selling_Price'])
+print("coefficient de Pearson = {}".format(coeff_pearson))
+# coeff_pearson : 0.03 --> Pas de correlation 
+
+coeff_pearson,_ = pearsonr(df['Kms_Driven'],df['Present_Price'])
+print("coefficient de Pearson = {}".format(coeff_pearson))
+# coeff_pearson : 0.20 --> Faible correlation 
